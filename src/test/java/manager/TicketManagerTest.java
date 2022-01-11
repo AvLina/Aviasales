27390c10d7fa557c017fa2238faf355a4d4e4eef
AvLina <avlina3@gmail.com>
2022-01-11 17:34:31 +0300

    Update to sorting testing

--- src/test/java/manager/TicketManagerTest.java
+++ src/test/java/manager/TicketManagerTest.java
@@ -4,9 +4,6 @@ import domain.Ticket;
 import org.junit.jupiter.api.Test;
 import repository.TicketRepository;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicketManagerTest {
@@ -16,6 +13,7 @@ class TicketManagerTest {
     Ticket ticket3 = new Ticket(30, 250, "DME", "ARV", 4000);
     Ticket ticket4 = new Ticket(40, 450, "AXG", "AXJ", 6000);
     Ticket ticket5 = new Ticket(50, 300, "AXD", "AZP", 3000);
+    Ticket ticket6 = new Ticket(60, 400, "DME", "AXJ", 6000);
 
     @Test
     public void findAllTickets () {
@@ -28,8 +26,9 @@ class TicketManagerTest {
         manager.add(ticket3);
         manager.add(ticket4);
         manager.add(ticket5);
+        manager.add(ticket6);
 
-        Ticket[] expected = {ticket1, ticket2, ticket3, ticket4, ticket5};
+        Ticket[] expected = {ticket1, ticket2, ticket3, ticket4, ticket5, ticket6};
         Ticket[] actual = repository.findAll();
 
         assertArrayEquals(expected, actual);
@@ -39,18 +38,17 @@ class TicketManagerTest {
     public void shouldFromTo () {
 
         TicketRepository repository = new TicketRepository();
-        repository.save(ticket1);
-        repository.save(ticket2);
-        repository.save(ticket3);
-        repository.save(ticket4);
         repository.save(ticket5);
+        repository.save(ticket2);
+        repository.save(ticket1);
+        repository.save(ticket4);
+        repository.save(ticket6);
+        repository.save(ticket3);
 
         TicketManager manager = new TicketManager(repository);
 
         Ticket[] expected = {ticket1, ticket3};
         Ticket[] actual = manager.searchTicket("DME", "ARV");
-
-        Arrays.sort(actual);
 
         assertArrayEquals(expected, actual);
     }
