2f73eb575b841eb55131e0722ada4033d56e37bc
AvLina <avlina3@gmail.com>
2022-01-18 12:12:49 +0300

    Adding an alignment test

--- src/test/java/manager/TicketManagerTest.java
+++ src/test/java/manager/TicketManagerTest.java
@@ -4,33 +4,26 @@ import domain.Ticket;
 import org.junit.jupiter.api.Test;
 import repository.TicketRepository;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicketManagerTest {
 
     Ticket ticket1 = new Ticket(10,200, "DME", "ARV", 1000);
-    Ticket ticket2 = new Ticket(20, 300, "ABT", "ABX", 10000);
+    Ticket ticket2 = new Ticket(20, 330, "ABT", "ABX", 10000);
     Ticket ticket3 = new Ticket(30, 250, "DME", "ARV", 4000);
     Ticket ticket4 = new Ticket(40, 450, "AXG", "AXJ", 6000);
     Ticket ticket5 = new Ticket(50, 300, "AXD", "AZP", 3000);
     Ticket ticket6 = new Ticket(60, 400, "DME", "AXJ", 6000);
 
     @Test
-    public void findAllTickets () {
+    public void shouldSortByPrice () {
 
-        TicketRepository repository = new TicketRepository();
-        TicketManager manager = new TicketManager(repository);
+        Ticket[] expected = new Ticket[] {ticket1, ticket3, ticket5, ticket2, ticket6, ticket4};
+        Ticket[] actual = new Ticket[] {ticket1, ticket2, ticket3, ticket4, ticket5, ticket6};
 
-        manager.add(ticket1);
-        manager.add(ticket2);
-        manager.add(ticket3);
-        manager.add(ticket4);
-        manager.add(ticket5);
-        manager.add(ticket6);
-
-        Ticket[] expected = {ticket1, ticket2, ticket3, ticket4, ticket5, ticket6};
-        Ticket[] actual = repository.findAll();
-
+        Arrays.sort(actual);
         assertArrayEquals(expected, actual);
     }
 
